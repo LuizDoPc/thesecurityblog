386bbef8ad8b0957ece8c867e4ae9f77f5e3cf66
luizdopc <luizotaviosoares@outlook.com>
2022-04-15 13:22:35 -0300

    create post page

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,7 +1,50 @@
+import { Button, Card, Form, Input } from 'antd';
+import Title from 'antd/lib/typography/Title';
+import './CreatePost.css'
+import { useNavigate } from 'react-router-dom';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { createPost } from '../services/post';
+import { useState } from 'react';
+
 export const CreatePost = () => {
-    return (
-        <div>
-            <h1>Create Post</h1>
-        </div>
-    )
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    await createPost(values);
+    setLoading(false);
+
+    navigate('/');
+  }
+
+  return (
+    <div id='container'>
+      <Card
+        style={{ width: '100%', marginBottom: 50 }}
+      >
+        <Title level={1}>The Security Blog</Title>
+        <Button type="primary" icon={<ArrowLeftOutlined />} onClick={() => {
+          navigate('/');
+        }}>
+          Home
+        </Button>
+      </Card>
+
+      <Form layout="vertical" name="nest-messages" onFinish={onFinish} id="form">
+        <Form.Item name="title" label="Title" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+
+        <Form.Item name="content" label="Content" rules={[{ required: true }]}>
+          <Input.TextArea size='large' />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
